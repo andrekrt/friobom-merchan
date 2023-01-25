606c45a96cc2178198465792beb76e3775f3f78a
andrekrt <andrekrt1922@gmail.com>
2023-01-25 15:07:56 -0300

    validar estoque

--- material/add-saida-material.php
+++ material/add-saida-material.php
@@ -16,26 +16,39 @@ if(isset($_SESSION['idusuario']) && empty($_SESSION['idusuario'])==false){
     $usuario = $_SESSION['idusuario'];
     $promotor = filter_input(INPUT_POST, 'promotor');
 
-    // echo "$usuario<br>$material<br>$fornecedor[0]<br>$dataSaida<br>$qtd<br>$cliente<br>$rota";
-
-    $sql = $db->prepare("INSERT INTO saidas (data_saida, material, industria,  qtd, cliente, rota, promotor, usuario) VALUES (:saida, :material, :industria, :qtd, :cliente, :rota,:promotor, :usuario)");
-    $sql->bindValue(':saida', $dataSaida);
-    $sql->bindValue(':material', $material);
-    $sql->bindValue(':industria', $fornecedor[0]);
-    $sql->bindValue(':qtd', $qtd);
-    $sql->bindValue(':cliente', $cliente);
-    $sql->bindValue(':rota', $rota);
-    $sql->bindValue(':promotor', $promotor);
-    $sql->bindValue(':usuario', $usuario);
-    
-    if($sql->execute()){
-        contaEstoque($material);
-        echo "<script> alert('Saida Lançada!!!')</script>";
+    //verifica qtd no estoque
+    $consulta = $db->prepare("SELECT total_estoque FROM material WHERE idmaterial = :material LIMIT 1");
+    $consulta->bindValue(':material', $material);
+    $consulta->execute();
+    $estoqueAtual = $consulta->fetch();
+    $estoqueAtual = $estoqueAtual['total_estoque'];
+    if($qtd>$estoqueAtual){
+        echo "<script> alert('Estoque Insuficiente!!!')</script>";
         echo "<script> window.location.href='saidas.php' </script>";
-        
     }else{
-        print_r($sql->errorInfo());
+        // echo "$usuario<br>$material<br>$fornecedor[0]<br>$dataSaida<br>$qtd<br>$cliente<br>$rota";
+
+        $sql = $db->prepare("INSERT INTO saidas (data_saida, material, industria,  qtd, cliente, rota, promotor, usuario) VALUES (:saida, :material, :industria, :qtd, :cliente, :rota,:promotor, :usuario)");
+        $sql->bindValue(':saida', $dataSaida);
+        $sql->bindValue(':material', $material);
+        $sql->bindValue(':industria', $fornecedor[0]);
+        $sql->bindValue(':qtd', $qtd);
+        $sql->bindValue(':cliente', $cliente);
+        $sql->bindValue(':rota', $rota);
+        $sql->bindValue(':promotor', $promotor);
+        $sql->bindValue(':usuario', $usuario);
+        
+        if($sql->execute()){
+            contaEstoque($material);
+            echo "<script> alert('Saida Lançada!!!')</script>";
+            echo "<script> window.location.href='saidas.php' </script>";
+            
+        }else{
+            print_r($sql->errorInfo());
+        }
     }
+
+    
 
 }else{
     echo "<script> alert('Acesso não permitido')</script>";
